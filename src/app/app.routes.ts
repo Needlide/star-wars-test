5893e2b506fc362660c1e487fc3301d508a1dc30
Needlide <max.litvinyuck553@gmail.com>
2024-10-07 06:21:06 +0000

    Fixed missing path and missing characters

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MovieListComponent } from './movie/components/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movie/components/movie-details/movie-details.component';
 
 export const routes: Routes = [
-  { path: '', component: MovieListComponent }
+  { path: '', component: MovieListComponent },
+  { path: 'movie/:id', component: MovieDetailsComponent }
 ];
--- src/app/movie/components/movie-details/movie-details.component.ts
+++ src/app/movie/components/movie-details/movie-details.component.ts
@@ -1,22 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../../models/movie.model';
 import { CachingService } from '../../../caching.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Starship } from '../../../starship/starship.model';
 import { forkJoin } from 'rxjs';
 import { Vehicle } from '../../../vehicle/vehicle.model';
 import { Planet } from '../../../planet/planet.model';
 import { Species } from '../../../species/species.model';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { Character } from '../../../character/models/character.model';
 
 @Component({
   selector: 'app-movie-details',
   standalone: true,
-  imports: [],
+  imports: [
+    MatProgressSpinnerModule,
+    CommonModule,
+    RouterModule,
+    MatCardModule,
+    MatListModule,
+    MatExpansionModule
+  ],
   templateUrl: './movie-details.component.html',
   styleUrl: './movie-details.component.scss'
 })
 export class MovieDetailsComponent implements OnInit {
   movie: Movie | undefined;
+  characters: Character[] = [];
   starships: Starship[] = [];
   vehicles: Vehicle[] = [];
   planets: Planet[] = [];
@@ -37,12 +51,14 @@ export class MovieDetailsComponent implements OnInit {
     this.movie = movie;
 
       forkJoin({
+        characters: forkJoin(movie.characters.map(url => this.cache.getCharacterByUrl(url))),
         starships: forkJoin(movie.starships.map(url => this.cache.getStarshipByUrl(url))),
         vehicles: forkJoin(movie.vehicles.map(url => this.cache.getVehicleByUrl(url))),
         planets: forkJoin(movie.planets.map(url => this.cache.getPlanetByUrl(url))),
         species: forkJoin(movie.species.map(url => this.cache.getSpeciesByUrl(url)))
       }).subscribe({
         next: (relatedData) => {
+          this.characters = relatedData.characters;
           this.starships = relatedData.starships;
           this.vehicles = relatedData.vehicles;
           this.planets = relatedData.planets;
