c10d9ee8684d7fb661515290a912c45864e014ac
Needlide <max.litvinyuck553@gmail.com>
2024-10-07 07:34:01 +0000

    Removed unnecessary unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('star-wars-test-project');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, star-wars-test-project');
-  });
 });
