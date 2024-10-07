9e4d0a6652647b871ea0e70bfab9ded4527f221f
Needlide <max.litvinyuck553@gmail.com>
2024-10-07 06:32:28 +0000

    Added new path for CharacterDetails component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MovieListComponent } from './movie/components/movie-list/movie-list.component';
 import { MovieDetailsComponent } from './movie/components/movie-details/movie-details.component';
+import { CharacterDetailsComponent } from './character/components/character-details/character-details.component';
 
 export const routes: Routes = [
   { path: '', component: MovieListComponent },
-  { path: 'movie/:id', component: MovieDetailsComponent }
+  { path: 'movie/:id', component: MovieDetailsComponent },
+  { path: 'character/:id', component: CharacterDetailsComponent }
 ];
