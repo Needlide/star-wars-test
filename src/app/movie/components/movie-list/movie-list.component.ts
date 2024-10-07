25d805b97430b1c1ef1415ff00d0b5b2eadd9bff
Needlide <max.litvinyuck553@gmail.com>
2024-10-07 05:51:07 +0000

    Implemented main page

--- src/app/movie/components/movie-list/movie-list.component.ts
+++ src/app/movie/components/movie-list/movie-list.component.ts
@@ -1,12 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Movie } from '../../models/movie.model';
+import { CachingService } from '../../../caching.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatListModule,
+    MatProgressSpinnerModule,
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.scss'
 })
 export class MovieListComponent {
+  movies: Movie[] = [];
+  isLoading = false;
 
+  constructor(private cache: CachingService) {
+    this.fetchMovies();
+  }
+
+  fetchMovies(): void {
+    this.isLoading = true;
+
+    const movieRequests = [];
+
+    for (let id = 1; id <= 6; id++) {
+      movieRequests.push(this.cache.getMovieById(id));
+    }
+
+    forkJoin(movieRequests).subscribe({
+      next: (movies) => {
+        this.movies = movies;
+        this.isLoading = false;
+      },
+      error: (e) => {
+        console.error('Error fetching movies:', e);
+        this.isLoading = false;
+      }
+    });
+  }
 }
